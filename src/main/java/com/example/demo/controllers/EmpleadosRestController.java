c8fe8d772bc7f83948b3bc22f7185ae54eb8004d
hanoli <hanoli@hotmail.com>
2020-11-05 18:09:49 -0600

    Se integro Swagger al proyecto

--- src/main/java/com/example/demo/controllers/EmpleadosRestController.java
+++ src/main/java/com/example/demo/controllers/EmpleadosRestController.java
@@ -2,30 +2,42 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Empleado;
 import com.example.demo.services.IEmpleadoServiceImpl;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/api")
+@Api
 public class EmpleadosRestController {
 
 	@Autowired
 	private IEmpleadoServiceImpl empleadosImpl;
 	
+	@ApiOperation(value = "Is Alive operation", notes = "Encuentra un empleado por su ID")
+	@ApiResponses({@ApiResponse(code = HttpServletResponse.SC_OK, message = "OK"),
+	@ApiResponse(code = HttpServletResponse.SC_INTERNAL_SERVER_ERROR, message = "INTERNAL ERROR SERVER"),
+	@ApiResponse(code = HttpServletResponse.SC_UNAUTHORIZED, message = "UNAUTHORIZED"),
+	@ApiResponse(code = HttpServletResponse.SC_FORBIDDEN, message = "FORBIDDEN"),
+	@ApiResponse(code = HttpServletResponse.SC_NOT_FOUND, message = "ELEMENTO NOT FOUND")})
 	@GetMapping("/lista")
 	public List<Empleado> getEmpleados(){
 		return empleadosImpl.getLista();
 	}
 	
-	@GetMapping("/idempleado/{id}")
+	@GetMapping("/idEmpleado/{id}")
 	public Empleado getEmpleadoById(@PathVariable Long id){
 		return empleadosImpl.empleadoId(id);
 	}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,4 @@
 insert into empleados (nombre,apellido) values ('han','thom');
+insert into empleados (nombre,apellido) values ('thom','han');
+insert into empleados (nombre,apellido) values ('hanoli','thom');
+insert into empleados (nombre,apellido) values ('myhanoli','thom');
